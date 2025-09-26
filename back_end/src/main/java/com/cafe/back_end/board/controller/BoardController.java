26ef64eb7076928972c84525df234a0c1d99134d
LeeTaeWon <ltw0781@naver.com>
2025-09-27 00:18:01 +0900

    게시판 기능 개선 및 HTML 템플릿 추가

--- back_end/src/main/java/com/cafe/back_end/board/controller/BoardController.java
+++ back_end/src/main/java/com/cafe/back_end/board/controller/BoardController.java
@@ -3,92 +3,55 @@ package com.cafe.back_end.board.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.cafe.back_end.board.dto.Board;
 import com.cafe.back_end.board.service.BoardService;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @Slf4j
-@RestController
-@CrossOrigin(origins = "*")
+@Controller
 @RequestMapping("/boards")
 public class BoardController {
 
     @Autowired
     private BoardService boardService;
 
-    // 게시글 목록
-    @GetMapping()
-    public ResponseEntity<?> getAll() {
-        try {
-            List<Board> boardList = boardService.list();
-            return new ResponseEntity<>(boardList, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    /**
+     * 게시판 목록
+     * @param model
+     * @throws Exception
+     */
+    @GetMapping("/list")
+    public void list(Model model) throws Exception {
+        List<Board> boardList = boardService.list();
+        model.addAttribute("boardList", boardList);
     }
+    
+    /**
+     * 게시판 상세
+     * @param no
+     * @param model
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/read")
+    public String read(@RequestParam("no") int no, Model model) throws Exception {
 
-    @GetMapping("/{no}")
-    public ResponseEntity<?> getOne(@PathVariable("no") int no) {
-        try {
-            Board board = boardService.select(no);
-            return new ResponseEntity<>(board, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+        Board board = boardService.select(no);
+        model.addAttribute("board", board);
+        return "boards/read";
 
-    @PostMapping()
-    public ResponseEntity<?> create(@RequestBody Board board) {
-        try {
-            Board newBoard = boardService.insert(board);
-            if(newBoard != null)
-                return new ResponseEntity<>(newBoard, HttpStatus.OK);
-            else
-                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
-
-    @PutMapping()
-    public ResponseEntity<?> update(@RequestBody Board board) {
-        try {
-            int result = boardService.update(board);
-            if(result > 0)
-                return new ResponseEntity<>("Update Result", HttpStatus.OK);
-            else
-                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-    @DeleteMapping("/{no}")
-    public ResponseEntity<?> destroy(@PathVariable("no") int no) {
-        try {
-            int result = boardService.delete(no);
-            if(result > 0)
-                return new ResponseEntity<>("Delete Result", HttpStatus.OK);
-            else
-                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
+    
 
     
     
