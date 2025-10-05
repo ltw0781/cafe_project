da35dadb314e7dcc4203b7e2456a56cf7e27d81f
LeeTaeWon <ltw0781@naver.com>
2025-10-05 19:29:35 +0900

    헤더 영역 스타일 개선 및 로고 이미지 경로 수정, 슬라이드 이미지 경로 변경

--- back_end/src/main/java/com/cafe/back_end/board/service/BoardServiceImpl.java
+++ back_end/src/main/java/com/cafe/back_end/board/service/BoardServiceImpl.java
@@ -38,7 +38,7 @@ public class BoardServiceImpl implements BoardService{
 
         int result = boardMapper.insert(board);
         return result;
-        
+
     }
 
     @Override
@@ -56,6 +56,5 @@ public class BoardServiceImpl implements BoardService{
         return result;
 
     }
-    
-    
+
 }
